a24cd65400b76639c80d6edb5274ec8671d55d5a
ferggz <ferggz.dev@gmail.com>
2026-05-21 19:53:47 +0200

    CORS for prod frontend

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://job-tracker-fullstack-three.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
